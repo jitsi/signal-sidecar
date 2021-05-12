f739550d639a814a04654b3c01822b40e8c899bc
Scott Boone <scott.boone@8x8.com>
2021-05-12 12:11:19 -0500

    WIP: node + typescript rigging, basic checks added

---
+++ src/health_collector.ts
@@ -0,0 +1,129 @@
+import logger from './logger';
+import got, { CancelableRequest } from 'got';
+import { readFileSync } from 'fs';
+
+export interface HealthCollectorOptions {
+    jicofoHealthUrl: string;
+    prosodyHealthUrl: string;
+    statusFilePath: string;
+    healthPollingInterval: number;
+}
+
+export interface HealthReport {
+    health: string;
+    status: string;
+    services: {
+        jicofoReachable: boolean;
+        jicofoStatusCode: number;
+        prosodyReachable: boolean;
+        prosodyStatusCode: number;
+        statusFileFound: boolean;
+        statusFileContents: string;
+    }
+}
+
+export interface HttpCheck {
+    reachable: boolean;
+    statusCode: number;
+}
+
+export interface FileCheck {
+    readable: boolean;
+    contents: string;
+}
+
+export default class HealthCollector {
+    private jicofoHealthUrl: string;
+    private prosodyHealthUrl: string;
+    private statusFilePath: string;
+    private healthPollingInterval: number;
+    private requestTimeout: number;
+    private requestRetryCount: number;
+
+    constructor(options: HealthCollectorOptions) {
+        this.jicofoHealthUrl = options.jicofoHealthUrl;
+        this.prosodyHealthUrl = options.prosodyHealthUrl;
+        this.statusFilePath = options.statusFilePath;
+        this.healthPollingInterval = options.healthPollingInterval;
+        this.requestTimeout = 3 * 1000;
+        this.requestRetryCount = 2;
+
+        this.readStatusFile = this.readStatusFile.bind(this);
+        this.checkHealthHttp = this.checkHealthHttp.bind(this);
+    }
+
+    async checkHealthHttp(url: string, method: string = 'GET'): Promise<HttpCheck> {
+        try {
+            const response = await got(url, {
+                responseType: 'text',
+                timeout: this.requestTimeout,
+                retry: this.requestRetryCount,
+                method: method,
+            });
+            return <HttpCheck>{
+                reachable: true,
+                statusCode: response.statusCode,
+            }
+        } catch (err) {
+            logger.debug('checkHealthHttp failed', { err, url: url });
+            return <HttpCheck>{
+                reachable: false,
+                statusCode: 0,
+            }
+        }
+    }
+
+    readStatusFile(filePath: string): FileCheck {
+        try {
+            const contents = readFileSync(filePath, {encoding: 'utf8', flag:'r'});
+            return <FileCheck>{
+                readable: true,
+                contents: contents,
+            }
+        } catch (err) {
+            logger.debug('readStatusFile failed', { err, path: filePath });
+            return <FileCheck>{
+                readable: false,
+                contents: "",
+            }
+        }
+    }
+    
+    async updateHealthReport(): Promise<HealthReport> {
+        let report: Partial<HealthReport> = {};
+
+        // spawn concurrent calls
+        const jicofoHealthResp = this.checkHealthHttp(this.jicofoHealthUrl);
+        const prosodyHealthResp = this.checkHealthHttp(this.prosodyHealthUrl, 'POST');
+
+        const jhr = await jicofoHealthResp;
+        report.services.jicofoReachable = jhr.reachable;
+        report.services.jicofoStatusCode = jhr.statusCode;
+
+        const phr = await prosodyHealthResp;
+        report.services.prosodyReachable = phr.reachable;
+        report.services.prosodyStatusCode = phr.statusCode;
+
+        const statusFileCheck = this.readStatusFile(this.statusFilePath);
+        if (statusFileCheck.readable) {
+            report.services.statusFileFound = true;
+            report.services.statusFileContents = statusFileCheck.contents;
+            report.status = statusFileCheck.contents;
+        } else {
+            report.services.statusFileFound = false;
+            report.services.statusFileContents = "not found"
+            report.status = "UNKNOWN"
+        }
+
+        if (report.services.jicofoReachable && report.services.jicofoStatusCode == 200 &&
+            report.services.prosodyReachable && report.services.prosodyStatusCode == 200 &&
+            report.services.statusFileFound) {
+                report.health == "UP";
+        } else {
+            report.health == "DOWN";
+        }
+
+        logger.debug('Stats report', { report });
+        return <HealthReport>(report);
+    }
+}
