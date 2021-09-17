31f04a99ff586deb28e3ff6fd7e90bda34ea0b1f
Scott Boone <scott.boone@8x8.com>
2021-09-17 16:50:51 +0000

    WIP: fixed json parsing issues

--- src/census_collector.ts
+++ src/census_collector.ts
@@ -3,8 +3,7 @@ import got from 'got';
 
 export interface CensusData {
     reachable: boolean;
-    code: number;
-    contents: string;
+    contents: CensusReport;
 }
 
 export interface RoomData {
@@ -14,7 +13,7 @@ export interface RoomData {
 }
 
 export interface CensusReport {
-    rooms: Array<RoomData>;
+    room_census: Array<RoomData>;
 }
 
 export interface CensusCollectorOptions {
@@ -34,66 +33,44 @@ export default class CensusCollector {
         this.censusHost = options.censusHost;
         this.requestTimeout = 3 * 1000;
         this.requestRetryCount = 2;
-
         this.updateCensusReport = this.updateCensusReport.bind(this);
     }
 
     async checkCensusHttp(url: string): Promise<CensusData> {
         logger.debug('pulling census data from: ' + url);
         try {
-            const response = await got.get(url, {
-                headers: {
-                    host: this.censusHost,
-                },
-                responseType: 'json',
-                timeout: this.requestTimeout,
-                retry: this.requestRetryCount,
-                throwHttpErrors: false,
-            });
+            const response = await got
+                .get(url, {
+                    headers: {
+                        host: this.censusHost,
+                    },
+                    responseType: 'json',
+                    timeout: this.requestTimeout,
+                    retry: this.requestRetryCount,
+                    throwHttpErrors: false,
+                })
+                .json<CensusReport>();
             return <CensusData>{
                 reachable: true,
-                code: response.statusCode,
-                contents: response.body,
+                contents: response,
             };
         } catch (err) {
-            logger.debug('checkCensusHttp failed', { err, url });
+            logger.warn('checkCensusHttp failed', { err, url });
             return <CensusData>{
                 reachable: false,
-                code: 0,
-                contents: '',
+                contents: null,
             };
         }
     }
 
-    validateCensusJSON(pcensus: string): Array<RoomData> {
-        try {
-            const jsonArray: RoomData[] = JSON.parse(pcensus)['room_census'];
-            return jsonArray;
-        } catch (err) {
-            logger.error('failed to parse census data', pcensus);
-            return null;
-        }
-    }
-
-    async updateCensusReport(): Promise<CensusReport> {
+    async updateCensusReport(): Promise<CensusReport> | undefined {
         this.checkCensusHttp(this.prosodyCensusUrl).then((results) => {
-            if (results.reachable && results.code == 200) {
-                const censusRoomData = this.validateCensusJSON(results.contents);
-                if (censusRoomData) {
-                    return <CensusReport>{
-                        rooms: censusRoomData,
-                    };
-                }
+            if (results.reachable) {
+                return results.contents;
             } else {
-                if (!results.reachable) {
-                    logger.error('room census endpoint unreachable');
-                } else {
-                    logger.error('room census endpoint bad status: ' + results.code);
-                }
+                logger.error('room census endpoint is unreachable or gave bad response');
             }
         });
-        return <CensusReport>{
-            rooms: [],
-        };
+        return undefined;
     }
 }
