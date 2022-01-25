e4e2d50c4702f22577d6b32bb3a57fa2fe33837d
Aaron van Meerten <aaron.van.meerten@gmail.com>
2022-01-25 10:38:59 -0600

    more syntax changes

--- src/health_collector.ts
+++ src/health_collector.ts
@@ -9,11 +9,23 @@ export interface HealthData {
     contents: string;
 }
 
+export interface StatusFileData {
+    readable: boolean;
+    contents: string;
+}
 export interface HealthReport {
     healthy: boolean;
     status: string;
     services: {
-        [name: string]: HealthData;
+        jicofoReachable: boolean;
+        jicofoStatusCode: number;
+        jicofoStatusContents: string;
+        jicofoStatsReachable: boolean;
+        jicofoStatsStatusCode: number;
+        prosodyReachable: boolean;
+        prosodyStatusCode: number;
+        statusFileFound: boolean;
+        statusFileContents: string;
     };
     stats: {
         jicofoParticipants: number;
@@ -73,6 +85,10 @@ export default class HealthCollector {
         };
     }
 
+    initHealthData() {
+        return <HealthData>{reachable: false, code: 0, contents: ''};
+    }
+
     async checkHealthHttp(url: string, method = 'GET'): Promise<HealthData> {
         logger.debug('health check of ' + url);
         try {
@@ -101,94 +117,112 @@ export default class HealthCollector {
         }
     }
 
-    async readStatusFile(filePath: string): Promise<HealthData> {
+    async readStatusFile(filePath: string): Promise<StatusFileData> {
         logger.debug('status file check of ' + filePath);
-
+        let readable = false;
+        let contents = '';
         try {
             const data = readFileSync(filePath, 'utf8');
-            return <HealthData>{
-                reachable: true,
-                code: 1,
-                contents: data.trim(),
-            };
+            readable = true;
+            contents = data.trim();
         } catch (err) {
             logger.warn('health_collector readStatusFile failed', { err, path: filePath });
-            return <HealthData>{
-                reachable: false,
-                code: 0,
-                contents: '',
-            };
+        }
+        return <StatusFileData>{
+            readable,
+            contents
+        };
+    }
+    unsettleStatusFile(item: PromiseSettledResult<StatusFileData>): StatusFileData {
+        if (item.status != 'fulfilled') {
+            logger.warn('Unsettled status file promise',{ item });
+            return <StatusFileData>{readable: false};
+        } else {
+            return <StatusFileData>item.value;
         }
     }
-
+    unsettleHealthData(item: PromiseSettledResult<HealthData>): HealthData {
+        if (item.status != 'fulfilled') {
+            logger.warn('Unsettled health data promise',{ item });
+            return this.initHealthData();
+        } else {
+            return <HealthData>item.value;
+        }
+    }
     async updateHealthReport(): Promise<HealthReport> {
         // spawn concurrent calls
-        const ccalls: Promise<HealthData>[] = [];
-        ccalls.push(this.checkHealthHttp(this.jicofoHealthUrl));
-        ccalls.push(this.checkHealthHttp(this.jicofoStatsUrl));
-        ccalls.push(this.checkHealthHttp(this.prosodyHealthUrl));
-        ccalls.push(this.readStatusFile(this.statusFilePath));
+        const settledResult = await Promise.allSettled([
+            this.checkHealthHttp(this.jicofoHealthUrl),
+            this.checkHealthHttp(this.jicofoStatsUrl),
+            this.checkHealthHttp(this.prosodyHealthUrl),
+            this.readStatusFile(this.statusFilePath)
+        ]);
+        //remove statusFileResult, would prefer to use .pop here but typescript doesn't likey
+        const [statusFileResult] = settledResult.splice(3,1).map(this.unsettleStatusFile);        
+        const [jicofoHealth, jicofoStats, prosodyHealth] = settledResult.map(this.unsettleHealthData);
 
-        return Promise.all(ccalls).then((results: HealthData[]) => {
-            const [jicofoHealth, jicofoStats, prosodyHealth, statusFileResult] = results;
-            let parsedStatsFlag = false;
-            let jicofoParticipants, jicofoConferences = 0;
-            try {
-                const parsedStats = JSON.parse(jicofoStats.contents);
-                jicofoParticipants = parsedStats['participants'];
-                jicofoConferences = parsedStats['conferences'];
-                parsedStatsFlag = true;
-            } catch (err) {
-                logger.warn('failed to parse jicofo stats json', { err, json: jicofoStats.contents });
+        let parsedStatsFlag = false;
+        let jicofoParticipants: number, jicofoConferences:number;
+        try {
+            const parsedStats = JSON.parse(jicofoStats.contents);
+            jicofoParticipants = parsedStats['participants'];
+            jicofoConferences = parsedStats['conferences'];
+            parsedStatsFlag = true;
+        } catch (err) {
+            logger.warn('failed to parse jicofo stats json', { err, json: jicofoStats.contents });
+        }
+
+        let overallhealth = false;
+        if (
+            jicofoHealth.reachable &&
+            jicofoHealth.code == 200 &&
+            jicofoStats.reachable &&
+            jicofoStats.code == 200 &&
+            prosodyHealth.reachable &&
+            prosodyHealth.code == 200 &&
+            statusFileResult.readable &&
+            parsedStatsFlag // stats file parsed successfully
+        ) {
+            overallhealth = true;
+        }
+
+        let overallstatus = statusFileResult.contents;
+        if (jicofoParticipants > this.participantMax) {
+            // TODO: log something here
+            overallstatus = 'drain';
+        }
+
+        const report = <HealthReport>{
+            healthy: overallhealth,
+            status: overallstatus,
+            services: {
+                jicofoReachable: jicofoHealth.reachable,
+                jicofoStatusCode: jicofoHealth.code,
+                jicofoStatusContents: jicofoHealth.contents,
+                jicofoStatsReachable: jicofoStats.reachable,
+                jicofoStatsStatusCode: jicofoStats.code,
+                prosodyReachable: prosodyHealth.reachable,
+                prosodyStatusCode: prosodyHealth.code,
+                statusFileFound: statusFileResult.readable,
+                statusFileContents: statusFileResult.contents,
+            },
+            stats: {
+                jicofoParticipants,
+                jicofoConferences,
+            },
+        };
+
+        if (!overallhealth) {
+            logger.warn('updateHealthReport returned unhealthy', { report });
+            if (this.collectMetrics) {
+                metrics.SignalHealthGauge.set(0);
             }
-
-            let overallhealth = false;
-            if (
-                jicofoHealth.reachable &&
-                jicofoHealth.code == 200 &&
-                jicofoStats.reachable &&
-                jicofoStats.code == 200 &&
-                prosodyHealth.reachable &&
-                prosodyHealth.code == 200 &&
-                statusFileResult.reachable &&
-                parsedStatsFlag // stats file parsed successfully
-            ) {
-                overallhealth = true;
+        } else {
+            logger.debug('updateHealthReport return', { report });
+            if (this.collectMetrics) {
+                metrics.SignalHealthGauge.set(1);
             }
-
-            let overallstatus = statusFileResult.contents;
-            if (jicofoParticipants > this.participantMax) {
-                // TODO: log something here
-                overallstatus = 'drain';
-            }
-
-            const report = <HealthReport>{
-                healthy: overallhealth,
-                status: overallstatus,
-                services: {
-                    jicofoHealth,
-                    jicofoStats,
-                    prosodyHealth,
-                    statusFileResult
-                },
-                stats: {
-                    jicofoParticipants,
-                    jicofoConferences,
-                },
-            };
-
-            if (!overallhealth) {
-                logger.warn('updateHealthReport returned unhealthy', { report });
-                if (this.collectMetrics) {
-                    metrics.SignalHealthGauge.set(0);
-                }
-            } else {
-                logger.debug('updateHealthReport return', { report });
-                if (this.collectMetrics) {
-                    metrics.SignalHealthGauge.set(1);
-                }
-            }
-            return report;
-        });
+        }
+        return report;
     }
 }
